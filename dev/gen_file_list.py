6217ff24573d406a8bbda9b389d3be5c664e2d6f
JarateKing <scmacintosh@upei.ca>
2018-10-02 14:01:33 -0300

    added filepath from headers

--- dev/gen_file_list.py
+++ dev/gen_file_list.py
@@ -2,11 +2,15 @@ import os
 
 # get all filenames
 existing = []
+current_path = ""
 f = open(os.path.abspath("../2-LISTS/Filelist.md"), "r")
 for line in f:
 	if "#" not in line:
 		if " | " in line and "File | Description" not in line and "---- | " not in line:
-			existing.append(line)
+			existing.append(current_path + line)
+	else:
+		if "/" in line:
+			current_path = line.replace("## ","").replace("\n","")
 f.close()
 
 # debug
